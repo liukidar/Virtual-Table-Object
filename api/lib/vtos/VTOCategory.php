1a3514d8aa3152c1abb1aed5d25ef93802e12e0e
Luca Pinchetti <pincoluca1@gmail.com>
2018-08-07 20:02:41 +0200

    Clean

--- api/lib/vtos/VTOCategory.php
+++
@@ -1,7 +0,0 @@
-<?php
-class VTOCategory extends VTO {
-    public function __construct() 
-    {
-    	parent::__construct('category', '_bookrewind_categories', []);
-    }
-}
--- api/lib/vtos/VTOUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-class VTOUser extends VTO {
-    public function __construct() 
-    {
-    	parent::__construct('user', '_usermng_userlist', [], [
-            'book' => ['book', 'book.owner = user.id']
-        ]);
-    }
-}
